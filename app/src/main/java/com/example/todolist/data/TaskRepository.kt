cc891ed0cc13dcdb559030a2fcae0c7af30a0b15
SANGWAMBALESarah <23sm515si@esisalama.org>
2026-04-02 10:22:17 +0200

    ajout de data

---
+++ app/src/main/java/com/example/todolist/data/TaskRepository.kt
@@ -0,0 +1,31 @@
+package com.example.todolist.data
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class TaskRepository {
+    private val _tasks = MutableStateFlow<List<Task>>(emptyList())
+    val tasks = _tasks.asStateFlow()
+
+    fun addTask(name: String) {
+        val task = Task(
+            id = _tasks.value.size + 1,
+            name = name,
+            isDone = false
+        )
+        _tasks.value = _tasks.value + task
+    }
+
+    fun updateTask(task: Task){
+        _tasks.value = _tasks.value.map {
+            _tasks.value = _tasks.value.map {
+                taskItem -> if (taskItem.id == task.id) {
+                    task.copy(isDone = task.isDone)
+            }
+                else{
+                    taskItem
+            }
+            }
+        }
+    }
+}
