cf8d83033172fed3ccb4b61eb45e28d9ef6babe6
SANGWAMBALESarah <23sm515si@esisalama.org>
2026-04-17 19:02:50 +0200

    Étape 7 : Modifier TaskViewModel et créer ViewModelFactory

--- app/src/main/java/com/example/todolist/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/example/todolist/viewmodel/TaskViewModel.kt
@@ -1,20 +1,31 @@
 package com.example.todolist.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.todolist.data.*
-import kotlinx.coroutines.flow.StateFlow
+import androidx.lifecycle.viewModelScope
+import com.example.todolist.data.repository.TaskRepository
+import com.example.todolist.model.Task
+import kotlinx.coroutines.launch
+import java.time.LocalDate
 
-class TaskViewModel : ViewModel() {
-    private val repository = TaskRepository()
-    val tasks : StateFlow<List<Task>> = repository.tasks
+class TaskViewModel(private val repository: TaskRepository) : ViewModel() {
 
-    fun addTask(name: String) {
-        repository.addTask(name)
+    val tasks = repository.tasks
+
+    fun addTask(name: String, dateDebut: LocalDate, dateFin: LocalDate) {
+        viewModelScope.launch {
+            repository.addTask(name, dateDebut, dateFin)
+        }
     }
 
     fun updateTask(task: Task) {
-        repository.updateTask(task)
+        viewModelScope.launch {
+            repository.updateTask(task)
+        }
     }
 
-
+    fun deleteTask(task: Task) {
+        viewModelScope.launch {
+            repository.deleteTask(task)
+        }
+    }
 }
